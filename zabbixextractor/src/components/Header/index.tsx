2b7e11dda6d527dac8b4f087f587f6fffead9770
NathanQSilva <72088284+NathanQSilva@users.noreply.github.com>
2021-05-18 00:04:27 -0300

    add head component
    Adicionado o componente de cabeçalho da aplicação

---
+++ zabbixextractor/src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export function Head() {
+    return (
+        <div>
+            header
+        </div>
+    )
+}
--- zabbixextractor/src/pages/_app.tsx
+++ zabbixextractor/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 // Styles
 import '../styles/globals.scss'
 
+// Internal imports
 import { LoginContextProvider } from '../contexts/LoginContext'
+import { Head } from '../components/Header/index'
 
 function MyApp({ Component, pageProps }) {
   return (
     <LoginContextProvider>
+      <Head />
       <Component { ...pageProps } />
     </LoginContextProvider>
   )
