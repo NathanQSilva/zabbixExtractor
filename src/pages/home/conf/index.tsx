18bc0f01f3a8e1710d765688e94d18c91bca46a0
NathanQSilva <72088284+NathanQSilva@users.noreply.github.com>
2021-05-25 20:56:28 -0300

    configuração apara alteração de usuário

--- src/pages/home/conf/index.tsx
+++ src/pages/home/conf/index.tsx
@@ -1,11 +1,64 @@
+//// External imports
+import { useFormik } from 'formik'
+
 // Internal imports
 import { Head } from '../../../components/Header/index';
+import { useLogin } from '../../../contexts/LoginContext'
 
 export default function ConfigHome() {
+    const {
+        GetLoginKey
+    } = useLogin()
+
+    const formik = useFormik({
+        initialValues: {
+          user: '', 
+          password: '',
+          server: '',
+        },
+    
+        // on submit the values are passed to the function saving only the key
+        onSubmit: values => {
+          GetLoginKey(values)
+        },
+    });
+
     return (
         <Head>
-            <div>
-                <h1>Confs</h1>
+            <div> 
+                <form onSubmit={formik.handleSubmit}>
+                    <div>
+                        <label>User</label>
+                        <input
+                            id="user"
+                            name="user"
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.user}
+                        />
+
+                        <label>Password</label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+
+                        <label>Server</label>
+                        <input
+                            id="server"
+                            name="server"
+                            type="text"
+                            onChange={formik.handleChange}
+                            value={formik.values.server}
+                        />
+                    </div>
+                    <div>
+                        <button type="submit">Save options</button>
+                    </div>
+                </form>
             </div>
         </Head>  
     )
