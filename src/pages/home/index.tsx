9c203b75817e85f5ff53bb9bf1a8f65bff11177a
NathanQSilva <nathanq_@hotmail.com>
2021-07-27 13:52:44 -0300

    Alteração no layout da aplicação. Fixes #12

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,17 @@
 // External imports
 import React, { useEffect } from 'react';
+import GridLayout from 'react-grid-layout';
+import Chart from '../../components/charts/chart'
 
 // Internal imports
 import { useRequests } from "../../contexts/RequestsContext"
 import { Head } from '../../components/Header/index';
-import styles from './index.module.scss'
 
-import Chart from '../../components/charts/chart'
+// Styles
+import styles from './index.module.scss'
+import "/node_modules/react-grid-layout/css/styles.css"
+import "/node_modules/react-resizable/css/styles.css"
+
 
 export default function homePage() {
     useEffect(() => {
@@ -17,6 +22,12 @@ export default function homePage() {
         HistoryGet,
         rawData
     } = useRequests()
+
+    const layout = [
+        {i: 'a', x: 0, y: 0, w: 1, h: 2},
+        {i: 'b', x: 1, y: 0, w: 3, h: 2},
+        {i: 'c', x: 4, y: 0, w: 1, h: 2}
+      ];
 
     function GenerateColor(opacity = 1) {
         let r = Math.random() * 255;
@@ -29,55 +40,47 @@ export default function homePage() {
     return (
         <Head>
             <div className={styles.homeContainer}>
-                <div className={styles.dashTop}>
-                    <Chart 
-                        labels={['1', '2', '3', '4', '5', '6']}
-                        graphName="teste 1"
-                        data={[1, 2, 4, 3, 7, 4]}
-                        color={GenerateColor()}
-                        borderColor={GenerateColor(0.6)}
-                    />
-                </div>
-                <div className={styles.dashLineTop}>
-                    <div className={styles.topLeft}>
-                        <Chart 
-                            labels={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-                            graphName="teste 2"
-                            data={[4, 2, 1, 7, 5, 9, 3, 5, 1, 2]}
-                            color={GenerateColor()}
-                            borderColor={GenerateColor(0.6)}
-                        />
+                <GridLayout
+                    className="layout" 
+                    layout={layout} 
+                    cols={12} 
+                    rowHeight={30} 
+                    width={1630}
+                >
+                    <div key="a">
+                        <div className={styles.graph}>
+                            <Chart 
+                                labels={['1', '2', '3', '4', '5', '6']}
+                                graphName="teste 1"
+                                data={[1, 2, 4, 3, 7, 4]}
+                                color={GenerateColor()}
+                                borderColor={GenerateColor(0.6)}
+                            />
+                        </div>
                     </div>
-                    <div className={styles.topRight}>
-                        <Chart 
-                            labels={['1', '2', '3', '4', '5', '6']}
-                            graphName="teste 3"
-                            data={[9, 8, 7, 9, 7, 10]}
-                            color={GenerateColor()}
-                            borderColor={GenerateColor(0.6)}
-                        />                       
+                    <div key="b">
+                        <div className={styles.graph}>
+                            <Chart 
+                                labels={['1', '2', '3', '4', '5', '6']}
+                                graphName="teste 1"
+                                data={[1, 2, 4, 3, 7, 4]}
+                                color={GenerateColor()}
+                                borderColor={GenerateColor(0.6)}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className={styles.dashLineBottom}>
-                    <div className={styles.bottomLeft}>
-                        <Chart
-                            labels={['1', '2', '3', '4', '5', '6']}
-                            graphName="teste 4"
-                            data={[8, 10, 6, 2, 8, 4]}
-                            color={GenerateColor()}
-                            borderColor={GenerateColor(0.6)}
-                        />
+                    <div key="c">
+                        <div className={styles.graph}>
+                            <Chart 
+                                labels={['1', '2', '3', '4', '5', '6']}
+                                graphName="teste 1"
+                                data={[1, 2, 4, 3, 7, 4]}
+                                color={GenerateColor()}
+                                borderColor={GenerateColor(0.6)}
+                            />
+                        </div>
                     </div>
-                    <div className={styles.bottomRight}>
-                        <Chart
-                            labels={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-                            graphName="teste 5"
-                            data={[7, 1, 10, 7, 7, 7, 4, 10, 3, 8]}
-                            color={GenerateColor()}
-                            borderColor={GenerateColor(0.6)}
-                        />
-                    </div>
-                </div>
+                </GridLayout>
             </div>
         </Head>
     )
